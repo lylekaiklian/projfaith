e02fbfa03cd49f65f6c3b185de4dd083cb83023d
Lyle <lyle.kaiklian@infoshiftinc.com>
2015-10-30 07:18:47 +0800

    my first commit

---
+++ js/anims.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {  
+	$('.dropdown').on('show.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+	});
+
+	// ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+});
